968f7d856c14f7f053fade2d668681350c3e5af9
Eldaras Zutautas <ez222eq@student.lnu.se>
2023-04-14 17:46:20 +0200

    First stages, added little design, first functionality

---
+++ app/src/main/java/cookbook/DBUtils.java
@@ -0,0 +1,97 @@
+package cookbook;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.*;
+
+public class DBUtils {
+
+    public static void changeScene(ActionEvent event, String fxmlFile, String username) {
+        Parent root = null;
+
+        if (username != null) {
+            try {
+                FXMLLoader loader = new FXMLLoader(DBUtils.class.getResource(fxmlFile));
+                root = loader.load();
+                LoginController loginController = loader.getController();
+                LoginController.setUserInformation(username);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            try {
+                root = FXMLLoader.load(DBUtils.class.getResource(fxmlFile));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root, 400, 600));
+        stage.show();
+    }
+
+    public static void logInUser(ActionEvent event, String username, String password) {
+        Connection connection = null;
+        PreparedStatement psInsert = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = DriverManager.getConnection("", "root", ""); // need a link to a db
+            psInsert = connection.prepareStatement(""); // SELECT password from table in db
+            psInsert.setString(1, username);
+            resultSet = psInsert.executeQuery();
+
+            if (!resultSet.isBeforeFirst()) {
+                System.out.println("User does not exist in database");
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("Provided username or password is incorrect");
+                alert.show();
+            } else {
+                while (resultSet.next()) {
+                    String retrievedPassword = resultSet.getString("password"); // gets password column
+                    if (retrievedPassword.equals(password)) {
+                        changeScene(event, "primary-stage", username);
+                    }
+                    else {
+                        System.out.println("Incorrect password");
+                        Alert alert = new Alert(Alert.AlertType.ERROR);
+                        alert.setContentText("The provided password is incorrect");
+                        alert.show();
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (psInsert != null) {
+                try {
+                    psInsert.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
