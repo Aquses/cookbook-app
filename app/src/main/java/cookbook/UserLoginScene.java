e9af609d0ad6d9655ac0f5e40113b2f2904c6fe4
Samah Diab <samahdiab565@gmail.com>
2023-04-23 20:43:13 +0200

    test

--- app/src/main/java/cookbook/UserLoginScene.java
+++ app/src/main/java/cookbook/UserLoginScene.java
@@ -96,7 +96,8 @@ private static void login() throws SQLException {
   private static boolean checkCredentials(String username, String password) {
     boolean Credentials = false;
     try {
-      Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/cookbook?user=root&password=123456&useSSL=false");   
+      Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/cookbook?user=root&password=123456&useSSL=false"); 
+        
       Statement statement = conn.createStatement();
       String query = "SELECT * FROM users WHERE username = '"+ username+"' AND password = '"+ password+"';";
       ResultSet rs = statement.executeQuery(query);
