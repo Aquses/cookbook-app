0eb57b0b76abe8d1837630a775cbb32759db3435
Eldaras Zutautas <ez222eq@student.lnu.se>
2023-04-23 23:15:06 +0200

    removed unnecessary folders/files

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.cookbook.cookbook {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.cookbook.cookbook to javafx.fxml;
-    exports com.cookbook.cookbook;
-}
