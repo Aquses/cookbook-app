f82938f1d5ffccb341ff740900bb717fee7fc087
Victor Rydberg <vr222gw@student.lnu.se>
2023-05-08 19:04:56 +0200

    create initial messages fxml file and controller

---
+++ app/src/main/java/cookbook/view/MessagesScene.java
@@ -0,0 +1,15 @@
+package cookbook.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
+public class MessagesScene {
+
+    @FXML
+    private AnchorPane ap;
+
+    @FXML
+    private GridPane grid;
+
+}
