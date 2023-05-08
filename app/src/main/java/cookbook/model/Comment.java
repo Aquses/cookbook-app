54b717ef08aaf7660b2c0abf7e38a1c8e5b74cf4
Mohammed Muqdad Sami Aljuboori <ma225tf@student.lnu.se>
2023-05-08 21:29:50 +0200

    added getters and setters + Resultset method

--- app/src/main/java/cookbook/model/Comment.java
+++ app/src/main/java/cookbook/model/Comment.java
@@ -1,5 +1,7 @@
 package cookbook.model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -7,6 +9,32 @@ import java.util.Date;
 public class Comment {
   private String comment_text;
   private Date date;
+  private int id;
+  private int User_id;
+  private int recipe_id;
+
+  public Comment(ResultSet rt) throws SQLException {
+    setComment_text(rt.getString(4));
+    setDate(rt.getDate(5));
+    setId(rt.getInt(1));
+
+  }
+
+  // a method to set the date format for the date of comments
+  public Date StringToDate(String s) {
+
+    Date result = null;
+    try {
+      SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+      result = dateFormat.parse(s);
+    }
+
+    catch (ParseException e) {
+      e.printStackTrace();
+
+    }
+    return result;
+  }
 
   /**
    * @return the comment_text
@@ -34,6 +62,48 @@ public class Comment {
    */
   public void setDate(Date date) {
     this.date = date;
+  }
+
+  /**
+   * @return the id
+   */
+  public int getId() {
+    return id;
+  }
+
+  /**
+   * @param id the id to set
+   */
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  /**
+   * @return the user_id
+   */
+  public int getUser_id() {
+    return User_id;
+  }
+
+  /**
+   * @param user_id the user_id to set
+   */
+  public void setUser_id(int user_id) {
+    User_id = user_id;
+  }
+
+  /**
+   * @return the recipe_id
+   */
+  public int getRecipe_id() {
+    return recipe_id;
+  }
+
+  /**
+   * @param recipe_id the recipe_id to set
+   */
+  public void setRecipe_id(int recipe_id) {
+    this.recipe_id = recipe_id;
   }
 
 }
