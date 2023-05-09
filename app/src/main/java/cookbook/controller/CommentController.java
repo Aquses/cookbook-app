662a726e56a3a2b0f5fe72d22e00e116445b7b82
Mohammed Muqdad Sami Aljuboori <ma225tf@student.lnu.se>
2023-05-10 01:54:58 +0200

    added new controller for comments

---
+++ app/src/main/java/cookbook/controller/CommentController.java
@@ -0,0 +1,36 @@
+package cookbook.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+public class CommentController {
+
+  @FXML
+  private Button deleteButton;
+
+  @FXML
+  private Button editButton;
+
+  @FXML
+  private TextArea mycomment;
+
+  @FXML
+  private Label myDate;
+
+  @FXML
+  private Label myusername;
+
+  @FXML
+  void editComment(ActionEvent event) {
+
+  }
+
+  @FXML
+  void removeComment(ActionEvent event) {
+
+  }
+
+}
