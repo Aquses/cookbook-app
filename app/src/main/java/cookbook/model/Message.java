1eef268185f90393329294b1d25b281f96d2f69e
Victor Rydberg <vr222gw@student.lnu.se>
2023-05-10 21:31:33 +0200

    modify message class attribtues

--- app/src/main/java/cookbook/model/Message.java
+++ app/src/main/java/cookbook/model/Message.java
@@ -1,6 +1,7 @@
 package cookbook.model;
 
 import java.sql.Date;
+import java.sql.Timestamp;
 
 
 
@@ -10,14 +11,15 @@ public class Message {
     private int receiver_id;
     private String content;
     private int recipe_id;
-    private Date date_created; 
+    // private Date date_created; 
+    private Timestamp date_created; 
 
-    public Message(int message_id,int sender_id, int receiver_id, String content, int recipe_id, Date date_created) {
+    public Message(int message_id, int sender_id, int receiver_id, int recipe_id, String content, Timestamp date_created) {
+        this.message_id = message_id;
         this.sender_id = sender_id;
         this.receiver_id = receiver_id;
-        this.content = content;
-        this.message_id = message_id; 
         this.recipe_id = recipe_id;
+        this.content = content;
         this.date_created = date_created; 
     }
 
@@ -41,7 +43,11 @@ public class Message {
         return recipe_id;
     }
 
-    public Date getDateCreated() {
+    // public Date getDateCreated() {
+    //     return date_created;
+    // }
+
+    public Timestamp getDateCreated() {
         return date_created;
     }
 
@@ -65,7 +71,11 @@ public class Message {
         this.content = content;
     }
 
-    public void setDateCreated(Date date_created) {
+    // public void setDateCreated(Date date_created) {
+    //     this.date_created = date_created;
+    // }
+
+    public void setDateCreated(Timestamp date_created) {
         this.date_created = date_created;
     }
 }
