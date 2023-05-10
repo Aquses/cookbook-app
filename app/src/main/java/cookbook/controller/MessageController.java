50da12a99118097816720308b69a64dd0a0c2efc
Victor Rydberg <vr222gw@student.lnu.se>
2023-05-10 21:28:48 +0200

    add controller for message item

---
+++ app/src/main/java/cookbook/controller/MessageController.java
@@ -0,0 +1,107 @@
+package cookbook.controller;
+
+import cookbook.model.Message;
+import cookbook.model.Recipe;
+import javafx.animation.FadeTransition;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+
+public class MessageController {
+
+    @FXML
+    private ImageView FoodImage;
+
+    @FXML
+    private Label FoodItemDescription;
+
+    @FXML
+    private Label FoodItemNameWhite;
+
+    @FXML
+    private VBox NumberLabels;
+
+    @FXML
+    private Pane RecipeButton;
+
+    @FXML
+    private Pane RecipePane;
+
+    @FXML
+    private Label ServingsCount;
+
+    @FXML
+    private Label TimeTakenCount;
+
+    @FXML
+    private Label messageContent;
+
+    @FXML
+    private Label messageDate;
+
+    @FXML
+    private Label messageHeader;
+
+    Recipe recipe;
+    Message message;
+
+    @FXML
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+        this.FoodItemNameWhite.setText(recipe.getName());
+        this.ServingsCount.setText(String.valueOf(recipe.getServings()));
+        this.TimeTakenCount.setText(String.valueOf((recipe.getCookTime())));
+        this.FoodItemDescription.setText(recipe.getDescription());
+
+    }
+
+    public void setMessage(Message msg) {
+        this.message = msg;
+        messageContent.setText(message.getContent());
+        messageDate.setText(String.valueOf(message.getDateCreated()));
+    }
+
+
+    @FXML
+    void mouseHover(MouseEvent event) {
+        FadeTransition pane = new FadeTransition(Duration.millis(400), RecipePane);
+        pane.setFromValue(0);
+        pane.setToValue(0.6);
+        pane.play();
+
+        FadeTransition numbers = new FadeTransition(Duration.millis(200), NumberLabels);
+        numbers.setFromValue(1);
+        numbers.setToValue(0);
+        numbers.play();
+
+        FadeTransition desc = new FadeTransition(Duration.millis(300), FoodItemDescription);
+        desc.setFromValue(0);
+        desc.setToValue(1);
+        desc.play();
+
+    }
+
+    @FXML
+    void mouseHoverExit(MouseEvent event) {
+        FadeTransition pane = new FadeTransition(Duration.millis(400), RecipePane);
+        pane.setFromValue(0.6);
+        pane.setToValue(0);
+        pane.play();
+
+        FadeTransition numbers = new FadeTransition(Duration.millis(200), NumberLabels);
+        numbers.setFromValue(0);
+        numbers.setToValue(1);
+        numbers.play();
+
+        FadeTransition desc = new FadeTransition(Duration.millis(300), FoodItemDescription);
+        desc.setFromValue(1);
+        desc.setToValue(0);
+        desc.play();
+
+    }
+
+}
