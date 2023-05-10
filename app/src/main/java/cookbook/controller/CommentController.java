c34f374226022a754bd08335aaef6063a8af6dc9
Mohammed Muqdad Sami Aljuboori <ma225tf@student.lnu.se>
2023-05-10 11:51:16 +0200

    added fxml file and some changes to controller

--- app/src/main/java/cookbook/controller/CommentController.java
+++ app/src/main/java/cookbook/controller/CommentController.java
@@ -1,10 +1,19 @@
 package cookbook.controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import cookbook.model.Comment;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
 
 public class CommentController {
 
@@ -23,8 +32,20 @@ public class CommentController {
   @FXML
   private Label myusername;
 
+  private Comment comment; // Assume Comment class represents a comment object
+
+  private AnchorPane parentAnchorPane;
+
+  public void setData(Comment comment, AnchorPane parent) {
+    this.comment = comment;
+    parentAnchorPane = parent;
+    this.mycomment.setText(comment.getComment_text());
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    myDate.setText(dateFormat.format(comment.getDate()));
+  }
+
   @FXML
-  void editComment(ActionEvent event) {
+  void editComment(ActionEvent event) throws SQLException {
 
   }
 
