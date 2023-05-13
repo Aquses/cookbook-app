136a5d757d6ada38cea7e2991453d094cdb14b0e
Mohammed Muqdad Sami Aljuboori <ma225tf@student.lnu.se>
2023-05-13 23:33:48 +0200

    created setter for user_id

--- app/src/main/java/cookbook/model/WeeklyDinnerList.java
+++ app/src/main/java/cookbook/model/WeeklyDinnerList.java
@@ -33,4 +33,11 @@ public class WeeklyDinnerList {
     return user_id;
   }
 
+  /**
+   * @param user_id the user_id to set
+   */
+  public void setUser_id(int user_id) {
+    this.user_id = user_id;
+  }
+
 }
