8d011867e0cc51efe2b4fb64de888f5f8d8d40a2
Mohammed Muqdad Sami Aljuboori <ma225tf@student.lnu.se>
2023-05-13 23:30:21 +0200

    created object class + intialized attributes

---
+++ app/src/main/java/cookbook/model/WeeklyDinnerList.java
@@ -0,0 +1,15 @@
+package cookbook.model;
+// this is the object class for weekly dinner list feature
+
+import java.sql.Date;
+import java.sql.SQLException;
+
+import com.mysql.cj.protocol.Resultset;
+
+public class WeeklyDinnerList {
+  private int week_id;
+  private int user_id;
+  private Date start_Date;
+  private Date end_date;
+
+}
