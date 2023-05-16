10d76c471beebf0b4943a5b0bebe2b48b18bab51
Ali Shakouri <as227cg@student.lnu.se>
2023-05-17 01:15:28 +0200

    help desk/help scene view 2st

--- app/src/main/java/cookbook/view/HelpScene.java
+++ app/src/main/java/cookbook/view/HelpScene.java
@@ -46,3 +46,42 @@ public class HelpScene implements Initializable {
         return helpScene;
     }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        ObservableList<Help> helps = FXCollections.observableArrayList();
+        helps.add(new Help("Search feature", "/tutorials/search.png"));
+        helps.add(new Help("Favorites feature", "/tutorials/favorite.png"));
+        helps.add(new Help("Sending a recipe", "/tutorials/send-recipe.png"));
+        helps.add(new Help("Burger Menu", "/tutorials/burger-menu.png"));
+        helps.add(new Help("Expanded Burger Menu", "/tutorials/burger-menu-extended.png"));
+        helps.add(new Help("Login", "/tutorials/login.png"));
+
+        FilteredList<Help> filteredHelps = new FilteredList<>(helps, b -> true);
+        loadRecipes(helps);
+
+        RecipeSearchField.textProperty().addListener((observable, oldValue, newValue) -> {
+            filteredHelps.setPredicate(recipe -> {
+                // if no search value, display everything.
+                if (newValue.isEmpty() || newValue.isBlank() || newValue == null) {
+                    return true;
+                }
+
+                String searchKeyword = newValue.toLowerCase();
+
+                // Check if the recipe matches the search keyword
+                if (recipe.getName().toLowerCase().indexOf(searchKeyword) > -1) {
+                    return true; // found matches
+                }
+
+                return false; // no matches found
+            });
+
+            SortedList<Help> sortedHelps = new SortedList<>(filteredHelps);
+
+            grid.getChildren().clear();
+
+            loadRecipes(sortedHelps);
+        });
+
+    }
