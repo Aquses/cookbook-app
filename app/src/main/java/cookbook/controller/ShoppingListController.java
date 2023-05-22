20fc30a77bde2c5703d48c6a0e1f7aefd04eaa3c
Victor Rydberg <vr222gw@student.lnu.se>
2023-05-22 20:23:07 +0200

    create method to test shopping list item works

--- app/src/main/java/cookbook/controller/ShoppingListController.java
+++ app/src/main/java/cookbook/controller/ShoppingListController.java
@@ -1,5 +1,13 @@
 package cookbook.controller;
 
+import java.sql.SQLException;
+
+import cookbook.model.Ingredient;
+import cookbook.model.QueryMaker;
+import cookbook.model.Session;
+import cookbook.model.ShoppingList;
+import cookbook.model.User;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 
@@ -7,5 +15,37 @@ public class ShoppingListController {
 
     @FXML
     private AnchorPane ap;
+
+    private User user;
+    private ShoppingList shoppingList;
+
+    @FXML
+    public void initialize() {
+        // testing out if shopping list object works
+        this.user = Session.getCurrentUser();
+        testingShoppingList();
+
+        ObservableList<Ingredient> ingredientList = shoppingList.getIngredientList();
+
+        System.out.println("Testing if ingredients exist in this shopping list: ");
+        for (Ingredient i : ingredientList) {
+            System.out.println(i.getIngredientName() + " " + i.getMeasurement());   
+        }
+
+    
+    }
+
+    private void testingShoppingList() {
+
+        try {
+            QueryMaker qm = new QueryMaker();
+            ShoppingList shoppingList = qm.retrieveShoppingList(1, user);
+
+            this.shoppingList = shoppingList;
+        
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 
 }
