2d82b6c6a58e807794bef1d96e47a5bf36fa48aa
Victor Rydberg <vr222gw@student.lnu.se>
2023-05-22 23:05:25 +0200

    implement query methods to test functionality

--- app/src/main/java/cookbook/controller/ShoppingListController.java
+++ app/src/main/java/cookbook/controller/ShoppingListController.java
@@ -6,7 +6,9 @@ import cookbook.model.Ingredient;
 import cookbook.model.QueryMaker;
 import cookbook.model.Session;
 import cookbook.model.ShoppingList;
+import cookbook.model.ShoppingListItem;
 import cookbook.model.User;
+import cookbook.model.WeeklyDinnerList;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
@@ -18,21 +20,20 @@ public class ShoppingListController {
 
     private User user;
     private ShoppingList shoppingList;
+    private ObservableList<ShoppingListItem> shoppingListItems;
+    private WeeklyDinnerList weeklyPlan;
 
     @FXML
     public void initialize() {
-        // testing out if shopping list object works
         this.user = Session.getCurrentUser();
         testingShoppingList();
+        testingWeeklyPlan();
+        testingInsertItems();
 
-        ObservableList<Ingredient> ingredientList = shoppingList.getIngredientList();
-
-        System.out.println("Testing if ingredients exist in this shopping list: ");
-        for (Ingredient i : ingredientList) {
-            System.out.println(i.getIngredientName() + " " + i.getMeasurement());   
+        // Testing in console if ingredients are retrieved. Currently does not handle duplicates or combine qty
+        for (ShoppingListItem item : shoppingListItems) {
+            System.out.println(item.getIngredientName() + " " + item.getQty() + " " + item.getMeasurement());
         }
-
-    
     }
 
     private void testingShoppingList() {
@@ -40,12 +41,38 @@ public class ShoppingListController {
         try {
             QueryMaker qm = new QueryMaker();
             ShoppingList shoppingList = qm.retrieveShoppingList(1, user);
-
             this.shoppingList = shoppingList;
-        
+
+            ObservableList<ShoppingListItem> shoppingListItems = qm.retrieveShoppingListItems(1);
+            this.shoppingListItems = shoppingListItems;
         } catch (SQLException e) {
             System.out.println("Error: " + e.getMessage());
         }
     }
+
+    // This should eventually not be here. When the user selects a weekly plan to create a shopping list, the weeklyplan object should be passed into 
+    // the parameter of a method that should be used in this controller class. This will allow the inserting of shopping list items and the
+    // "retrieveShoppingWeeklyPlan()" method can be removed.
+
+    private void testingWeeklyPlan() {
+
+        try {
+            QueryMaker qm = new QueryMaker();
+            WeeklyDinnerList weeklyPlan = qm.retrieveShoppingWeeklyPlan(1, user);
+            this.weeklyPlan = weeklyPlan;
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private void testingInsertItems() {
+
+        try {
+            QueryMaker qm = new QueryMaker();
+            qm.insertShoppingListItems(weeklyPlan, shoppingList.getListId());
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }            
 
 }
