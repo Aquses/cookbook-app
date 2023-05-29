3718e1dc61c4af7a9bbc9d38bdd865d20e6acefe
Victor Rydberg <vr222gw@student.lnu.se>
2023-05-29 05:25:44 +0200

    fix checkstyle and add comments for messages scene

--- app/src/main/java/cookbook/view/MessagesScene.java
+++ app/src/main/java/cookbook/view/MessagesScene.java
@@ -17,140 +17,104 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Region;
 
+/**
+ * {@summary} Class responsible for managing and displaying messages sent to logged-in user. 
+ * Retrieves messages along with the associated recipes, and populates the interface with them.
+ */
 public class MessagesScene {
 
-    @FXML
-    private AnchorPane ap;
+  @FXML
+  private AnchorPane ap;
 
-    @FXML
-    private GridPane grid;
+  @FXML
+  private GridPane grid;
 
-    User user;
+  User user;
 
-    @FXML
-    public void initialize() {
-        User currentUser = Session.getCurrentUser();
-        this.user = currentUser;
-        
-
-        retrieveMessages();
-        retrieveRecipes();
-        loadMessages();
-    }
-
-    ObservableList<Message> messageList =  FXCollections.observableArrayList();
-    ObservableList<Recipe> recipeList = FXCollections.observableArrayList();
-
-    public void retrieveRecipes() {
-
-        try {
-            QueryMaker qm = new QueryMaker();
-            // ObservableList<Recipe> databaseRecipes = qm.retrieveMessageRecipes(2);
-            ObservableList<Recipe> databaseRecipes = qm.retrieveMessageRecipes(user);
-            this.recipeList = databaseRecipes;
-            
-        } catch (SQLException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
-
-    public void retrieveMessages() {
-        // User currentUser = Session.getCurrentUser();
-        // System.out.println(currentUser.getFname());
-        // System.out.println(currentUser.getUserId());
-        
-        try {
-            QueryMaker qm = new QueryMaker();
-            // ObservableList<Message> databaseMessages = qm.retrieveMessages(2);
-            ObservableList<Message> databaseMessages = qm.retrieveMessages(user);
-            this.messageList = databaseMessages;
-
-            // for (Message msg : databaseMessages) {
-            //     messageList.add(msg);
-            // }
-
-        } catch (SQLException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-
-    }
-
+  /**
+   * {@summary} Initializes the ui and functionality by retrieving and loading the messages
+   *  and associated recipes.
+   */
+  @FXML
+  public void initialize() {
+    User currentUser = Session.getCurrentUser();
+    this.user = currentUser;
     
+    retrieveMessages();
+    retrieveRecipes();
+    loadMessages();
+  }
 
-    private void loadMessages(){
-        int row = 1, col = 0;
+  ObservableList<Message> messageList =  FXCollections.observableArrayList();
+  ObservableList<Recipe> recipeList = FXCollections.observableArrayList();
 
-        for(int i=0; i < messageList.size(); i++){
-            FXMLLoader fxmlLoader = new FXMLLoader();
-            fxmlLoader.setLocation(getClass().getResource("/cookbook/MsgItem.fxml"));
-            AnchorPane anchorPane = null;
-            try {
-                anchorPane = fxmlLoader.load();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+  /**
+   * {@summary} Retrieves associated recipes to messages that have been sent to the user.
+   */
+  public void retrieveRecipes() {
+    try {
+      QueryMaker qm = new QueryMaker();
+      ObservableList<Recipe> databaseRecipes = qm.retrieveMessageRecipes(user);
+      this.recipeList = databaseRecipes;
+        
+    } catch (SQLException e) {
+      System.out.println("Error: " + e.getMessage());
+    }
+  }
 
-            MessageController mc = fxmlLoader.getController();
-            
-            mc.setMessage(messageList.get(i), ap);
-            // mc.setData(allRecipes.get(i), ap);
-            mc.setRecipe(recipeList.get(i));
-            mc.setSenderName(messageList.get(i));
+  /**
+   * {@summary} Retrieve messages that have been sent to the user.
+   */
+  public void retrieveMessages() {
+    try {
+      QueryMaker qm = new QueryMaker();
+      ObservableList<Message> databaseMessages = qm.retrieveMessages(user);
+      this.messageList = databaseMessages;
 
-            if(col == 1){
-                col = 0;
-                row++;
-            }
-
-            grid.add(anchorPane, col++, row);
-            grid.setMinWidth(Region.USE_COMPUTED_SIZE);
-            grid.setPrefWidth(Region.USE_COMPUTED_SIZE);
-            grid.setMaxWidth(Region.USE_PREF_SIZE);
-
-            grid.setMinHeight(Region.USE_COMPUTED_SIZE);
-            grid.setPrefHeight(Region.USE_COMPUTED_SIZE);
-            grid.setMaxHeight(Region.USE_PREF_SIZE);
-
-            //GridPane.setMargin(anchorPane, new Insets(0,10,0,10));
-        }
+    } catch (SQLException e) {
+      System.out.println("Error: " + e.getMessage());
     }
 
-    // private void loadRecipes(ObservableList<Recipe> allRecipes){
-    //     int row = 1, col = 0;
+  }
 
-    //     for(int i=0; i<allRecipes.size(); i++){
-    //         FXMLLoader fxmlLoader = new FXMLLoader();
-    //         fxmlLoader.setLocation(getClass().getResource("/cookbook/RecipeItem.fxml"));
-    //         AnchorPane anchorPane = null;
-    //         try {
-    //             anchorPane = fxmlLoader.load();
-    //         } catch (IOException e) {
-    //             throw new RuntimeException(e);
-    //         }
+  /**
+   * {@summary} Loads the message item fxml containing message content and clickable attached recipe
+   * to a grid pane.
+   */
+  private void loadMessages() {
+    int row = 1;
+    int col = 0;
 
-    //         MessageController mc = fxmlLoader.getController();
-    //         itemController.setData(allRecipes.get(i), ap);
+    for (int i = 0; i < messageList.size(); i++) {
+      FXMLLoader fxmlLoader = new FXMLLoader();
+      fxmlLoader.setLocation(getClass().getResource("/cookbook/MsgItem.fxml"));
+      AnchorPane anchorPane = null;
+      try {
+        anchorPane = fxmlLoader.load();
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
 
-    //         if(col == 4){
-    //             col = 0;
-    //             row++;
-    //         }
+      MessageController mc = fxmlLoader.getController();
+      
+      mc.setMessage(messageList.get(i), ap);
+      // mc.setData(allRecipes.get(i), ap);
+      mc.setRecipe(recipeList.get(i));
+      mc.setSenderName(messageList.get(i));
 
-    //         grid.add(anchorPane, col++, row);
-    //         grid.setMinWidth(Region.USE_COMPUTED_SIZE);
-    //         grid.setPrefWidth(Region.USE_COMPUTED_SIZE);
-    //         grid.setMaxWidth(Region.USE_PREF_SIZE);
+      if (col == 1) {
+        col = 0;
+        row++;
+      }
 
-    //         grid.setMinHeight(Region.USE_COMPUTED_SIZE);
-    //         grid.setPrefHeight(Region.USE_COMPUTED_SIZE);
-    //         grid.setMaxHeight(Region.USE_PREF_SIZE);
+      grid.add(anchorPane, col++, row);
+      grid.setMinWidth(Region.USE_COMPUTED_SIZE);
+      grid.setPrefWidth(Region.USE_COMPUTED_SIZE);
+      grid.setMaxWidth(Region.USE_PREF_SIZE);
 
-    //         //GridPane.setMargin(anchorPane, new Insets(0,10,0,10));
-    //     }
-    // }
-
-
-
-    
-
+      grid.setMinHeight(Region.USE_COMPUTED_SIZE);
+      grid.setPrefHeight(Region.USE_COMPUTED_SIZE);
+      grid.setMaxHeight(Region.USE_PREF_SIZE);
+    }
+  }  
 }
